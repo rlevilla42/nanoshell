850f099c4706f648833f5edbb85c9c8bb3039eff
Raphael Levillain <rlevilla@c2r3s7.42lausanne.ch>
2023-09-16 22:11:00 +0200

    go faire des test mais c cense etre finito

--- mini_parsing.c
+++ mini_parsing.c
@@ -16,8 +16,8 @@ int	size_dollar(char **cmd, int i, int j)
 
 	temp = cmd[i];
 	size = 0;
-	while (temp[j] != 0 && (temp[j] < 9 && temp[j] > 12)
-			&& temp[j] != 32) // la a partir du $ dans char **cmd il check jusque la fin du mot tout simplement 
+	while (temp[j] != 0 && (temp[j] < 9 || temp[j] > 12)
+			&& temp[j] != 32) // la a partir du $ dans char **cmd il check jusque la fin du mot tout simplement (faut donc que ce soit differemt de NULL ET whitespace 
 	{
 		j++;
 		size++;
@@ -25,13 +25,15 @@ int	size_dollar(char **cmd, int i, int j)
 	return (size);
 }
 
-int	replace_dollar_by_find_value(char *value, char **cmd, int i, int j, int size)
+int	replace_dollar_by_find_value(char *value, char **cmd, int i, int size)
 {
-	int	size_bis;
 	int	k;
+	int	j;
 
-	size_bis = size + j;
 	k = 0;
+	j = 0;
+	while (cmd[i][j] != '$')
+		j++;
 	while (value[k] != '\0')
 	{
 		cmd[i][j] = value[k];
@@ -41,30 +43,32 @@ int	replace_dollar_by_find_value(char *value, char **cmd, int i, int j, int size
 	return (0);
 }
 
-int	find_name_cmd_env(char *temp, t_env *cmd_env)
+int	find_name_cmd_env(char *temp, t_env *cmd_env, int i, int size)
 {
-	int	check_exist_flag;
+	int		check_exist_flag;
 	t_env	*cmd_temp;
 
 	check_exist_flag = 0;
 	cmd_temp = cmd_env;
 	while (cmd_temp != NULL)
 	{
-		if (strncmp(temp, cmd_temp.name,size) == 0) // temp exist dans cmd_env.name !
+		if (strncmp(temp, cmd_temp.name, size) == 0) // temp exist dans cmd_env.name !
 		{
 			check_exist_flag = 1;
-			replace_dollar_by_find_value(value, cmd, i, j, size);
+			replace_dollar_by_find_value(cmd_temp.value, cmd, i, size);
 		}
-		cmd_temp = cmd_temp->next;
+		else
+			cmd_temp = cmd_temp->next;
 	}
+	return (0);
 }
 
 int	ft_dollars_parse(char **cmd)
 {
-	int		i;
-	int		j;
-	int		k;
-	int		size;
+	int			i;
+	int			j;
+	int			size;
+	char	*temp;
 	t_env	*cmd_env;
 
 	i = 0;
@@ -78,9 +82,11 @@ int	ft_dollars_parse(char **cmd)
 			if (cmd[i][j] == '$')
 			{
 				size = size_dollar(cmd, i, j);
+				if (size == 0)
+					return (0);
 				temp = ft_strndup(&cmd[i][j + 1], size); // dans strndup faire condition ou if size == 0 alors strndup = NULL
 													   // la je copie apres le $jusque la fin du mot
-
+				find_name_cmd_env(temp, cmd_env, i, size);
 				if (!temp)
 					return (0);
 			}
@@ -88,6 +94,7 @@ int	ft_dollars_parse(char **cmd)
 		}
 		i++;
 	}
+	return (0);
 }
 
 int	main(int argc, char **argv, char **envp)
